2f196e9c9f84e6ac15606b4b638bd25e6b3e19c8
Malihe Alavi <maliheface@gmail.com>
2022-03-06 14:21:34 +0330

    Update axios-api.js

--- src/axios-api.js
+++ src/axios-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'malihealavi.pythonanywhere.com',
+    baseURL: 'https://malihealavi.pythonanywhere.com/',
     timeout :1000,
     headers:{
         
